55ab7a9a0e6d664304545800a98226278e158db4
Thegd17 <115826780+Thegd17@users.noreply.github.com>
2023-07-22 22:16:25 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,4 @@
+document.querySelector(".menu-logo").addEventListener("click",function show() {
+    element = document.getElementById("slide-bar");
+    element.classList.toggle("slide-menu");
+});
